e9f9cfe214ae6be54874844f1f0deaf3f2e174fc
Orestis Tsioukis <orestistsioukis@gmail.com>
2019-06-13 15:05:55 +0300

    adding classList function

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const openMenu = document.getElementById('menuopenbutton');
-const closeMenu = document.getElementById('menuclosebutton');
+const openMenu = document.getElementById('openbutton');
+const closeMenu = document.getElementById('closebutton');
 const animMenu = document.getElementById('sidebarmenu');
 
-openMenu.addEventListener('click', function () {
-    animMenu.style.display = 'block';
+openMenu.addEventListener('click', function() {
+    animMenu.classList.add('show-menu');
 });
 
-closeMenu.addEventListener('click', function () {
-    animMenu.style.display = 'none';
+closeMenu.addEventListener('click', function() {
+    animMenu.classList.remove('show-menu');
 });
