9bd5a25cb20729134b35348019cb74b96bf4d778
Orestis Tsioukis <orestistsioukis@gmail.com>
2019-06-20 11:40:49 +0300

    toggle arrows when menu appears

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const load = document.getElementById('loading');
 const openMenu = document.getElementById('openbutton');
 const closeMenu = document.getElementById('closebutton');
 const animMenu = document.getElementById('sidebarmenu');
+const arrowLeft = document.getElementById('arrow-left');
+const arrowRight = document.getElementById('arrow-right');
 
 setTimeout(function() {
     load.style.display = 'none';
@@ -9,8 +11,12 @@ setTimeout(function() {
 
 openMenu.addEventListener('click', function() {
     animMenu.classList.add('show-menu');
+    arrowLeft.style.display = 'none';
+    arrowRight.style.display = 'none';
 });
 
 closeMenu.addEventListener('click', function() {
     animMenu.classList.remove('show-menu');
+    arrowLeft.style.display = 'block';
+    arrowRight.style.display = 'block';
 });
